9a4cf101e49c80e605f3e55db21292c63abf8b7e
Leroy <leroyhaveamailbox@gmail.com>
2023-01-17 18:24:57 +0100

    fixed build script?

--- build.py
+++ build.py
@@ -1,29 +1,30 @@
 from os.path import exists
+from subprocess import run
 from os import system
 from shutil import copytree, copy
-import sys
+from sys import exit
+
+cmd = {"lin": system, "win": run}
+exec = {"lin": "swap.out", "win": "swap.exe"}
+bad_exec = {"lin": "swap.exe", "win": "swap.out"}
 
 os = input("OS? (lin/win)")
-if os == "win" :
-    if not exists("swap.exe") or exists("swap.out") :
-        system("make clear")
-        system("make win")
-elif os == "lin" :
-    if not exists("swap.out") or exists("swap.exe") :
-        system("make clear")
-        system("make lin")
+if os in {"win", "lin"} :
+    if not exists(exec[os]) or exists(bad_exec[os]) :
+        cmd[os]("make clear")
+        cmd[os]("make win")
 else :
     print("unknown OS :(")
-    sys.exit()
+    exit()
 
 if exists("build") :
-    system("rm -r build")
-system("mkdir build")
+    cmd[os]("rm -r build")
+cmd[os]("mkdir build")
 
-system("mkdir build/saves")
+cmd[os]("mkdir build/saves")
 copy("saves/blank_save.json", "build/saves/blank_save.json")
 
-system("mkdir build/src")
+cmd[os]("mkdir build/src")
 copytree("src/include", "build/src/include")
 copytree("src/lib", "build/src/lib")
 
