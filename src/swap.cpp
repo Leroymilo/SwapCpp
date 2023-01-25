d0a023a427da99906517d6d0411d6ab93e4b4e1a
Leroy <leroyhaveamailbox@gmail.com>
2023-01-25 15:00:31 +0100

    made the game fullscreen with Vsync

--- src/swap.cpp
+++ src/swap.cpp
@@ -12,7 +12,9 @@
 int main()
 {
     // SFML initialization stuff :
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SWAP!");
+    // sf::RenderWindow window(sf::VideoMode(800, 800), "SWAP!");
+    sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "SWAP!", sf::Style::Fullscreen);  // For fullscreen
+    window.setVerticalSyncEnabled(true);
     sf::Font font;
     if (!font.loadFromFile("assets/font.ttf"))
         std::cout << "Could not load font" << std::endl;
